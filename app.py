4c82f7f469ba0ac99558bb42d87cd4016f2269ba
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-10-20 05:27:35 +0300

    Completed API request and sent it as a response to client  using imported requests and Response from FastAPI and Requests packages

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 import uvicorn
+import requests
 
 app = FastAPI()
 
 @app.get('/')
 async def index():
-    return {'msg':'Hello, World!'}
+    r = requests.get('https://v6.exchangerate-api.com/v6/1234456789/latest/USD')
+    return Response(r.content, status_code=200, media_type='application.json')
 
 if __name__ == '__main__':
     uvicorn.run('app:app', port=5555, log_level='info', reload=True)
