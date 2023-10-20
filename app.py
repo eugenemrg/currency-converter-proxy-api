7528a4fabc46844e634db67242093b288c61bebe
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-10-20 05:16:50 +0300

    Updated app to test output to client

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get('/')
 async def index():
-    print('Hello, World')
+    return {'msg':'Hello, World!'}
 
 if __name__ == '__main__':
     uvicorn.run('app:app', port=5555, log_level='info', reload=True)
