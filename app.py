3f684dab1de573c08d7d8e4ee8143e146646a580
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-10-20 05:37:49 +0300

    Fixed media type

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ APP_API_KEY = os.getenv('CONVERTER_API_KEY', None)
 @app.get('/')
 async def index():
     r = requests.get(f'https://v6.exchangerate-api.com/v6/{APP_API_KEY}/latest/USD')
-    return Response(r.content, status_code=200, media_type='application.json')
+    return Response(r.content, status_code=200, media_type='application/json')
 
 if __name__ == '__main__':
     uvicorn.run('app:app', port=5555, log_level='info', reload=True)
