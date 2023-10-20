e62c19e8ad3802e614f74304d5550eb35d3caf4b
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-10-20 05:58:38 +0300

    Added error handling for the request and response using FastAPI HTTPException

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, HTTPException
 import uvicorn
 import requests
 import os
@@ -9,8 +9,20 @@ APP_API_KEY = os.getenv('CONVERTER_API_KEY', None)
 
 @app.get('/')
 async def index():
-    r = requests.get(f'https://v6.exchangerate-api.com/v6/{APP_API_KEY}/latest/USD')
-    return Response(r.content, status_code=200, media_type='application/json')
+    if not APP_API_KEY:
+        raise HTTPException(
+            status_code=500,
+            detail='Server encountered an error and could unfortunately not complete the request'
+        )
+    else:
+        r = requests.get(f'https://v6.exchangerate-api.com/v6/{APP_API_KEY}/latest/USD')
+        if r.status_code == 200:
+            return Response(r.content, status_code=200, media_type='application/json')
+        else:
+            raise HTTPException(
+                status_code=502,
+                detail='Server is unable to complete the request at the moment'
+            )
 
 if __name__ == '__main__':
     uvicorn.run('app:app', port=5555, log_level='info', reload=True)
